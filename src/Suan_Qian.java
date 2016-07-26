721a969edca5e5c1de6367ccf6f2d7ba92592034
ylh96 <ylh96@10.0.0.243>
2016-07-26 00:25:54 -0700

    updated

--- src/Suan_Qian.java
+++ src/Suan_Qian.java
@@ -7,23 +7,26 @@
  *
  */
 
+import java.util.HashMap;
 import  java.util.Random ;
 
-public class Suan_Qian {
 
+
+public class Suan_Qian {
+	public static final String[] MEANING = {"very good", "good", "okay", "cautious", "very cautious"};
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-	}
-	
-	private int choose_stick(){
 		Random random = new Random();
+		HashMap<Integer, String> m = new HashMap<Integer, String>();
+		for (int i = 0; i < 100; i++) {
+			int luck = random.nextInt(5);
+			m.put(i, MEANING[luck]);
+		}
 		int num_sticks = random.nextInt(100);
-		
-		return num_sticks;
+		String result = m.get(num_sticks);
+		System.out.println("Your luck is: " + result);
 	}
-
 }
