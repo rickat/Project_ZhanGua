8dabf4a08e679a3471b1bce1f930c60731e1be7f
ylh96 <ylh96@10.0.0.243>
2016-07-31 23:17:34 -0700

    added file main

---
+++ src/Zhan_Gua_Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author ylh96
+ *
+ */
+public class Zhan_Gua_Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
